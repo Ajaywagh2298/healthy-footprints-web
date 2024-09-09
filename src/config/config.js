b407e34b3a6de63580a39ca12e1d9906b44bea88
Ajay Wagh <54326598+Ajaywagh2298@users.noreply.github.com>
2024-09-10 02:28:08 +0530

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,3 @@
-const BACKEND_HOST_URL = `https://healthy-footprints-server.vercel.app`;
+const BACKEND_HOST_URL = `https://8080-idx-healthy-footprints-servergit-1725914993667.cluster-bec2e4635ng44w7ed22sa22hes.cloudworkstations.dev`;
 
 export { BACKEND_HOST_URL };
